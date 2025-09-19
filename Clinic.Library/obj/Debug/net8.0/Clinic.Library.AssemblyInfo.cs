28502d088b75183842c8239faa81b5c1aa40480c
brendanboedy <brendanboedy@icloud.com>
2025-09-18 20:19:04 -0400

    renamed .sln

--- Clinic.Library/obj/Debug/net8.0/Clinic.Library.AssemblyInfo.cs
+++ Clinic.Library/obj/Debug/net8.0/Clinic.Library.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Clinic.Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22b4edf39794b309ed479469ca625c1059962660")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+474665e81596eefd3481a09c09dad678845116e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Clinic.Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Clinic.Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
