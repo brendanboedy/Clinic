6613b448bd20786a5e79ff05e94ff15fd06e70d1
brendanboedy <brendanboedy@icloud.com>
2025-11-04 08:31:09 -0500

    https format API

---
+++ Clinic.API/Clinic.API/Controllers/Database/FakeDatabase.cs
@@ -0,0 +1,12 @@
+using System;
+using Clinic.Library.Models;
+
+namespace Clinic.API.Controllers.Database;
+
+public static class FakeDatabase
+{
+    public static List<Patient> Patients = new List<Patient>
+    {
+
+    };
+}
--- Clinic.API/Clinic.API/Program.cs
+++ Clinic.API/Clinic.API/Program.cs
@@ -22,4 +22,6 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.MapGet("/", () => Results.Redirect("/swagger")).ExcludeFromDescription();
+
 app.Run();
